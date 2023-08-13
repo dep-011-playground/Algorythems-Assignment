d2f0c4c02c7533bbe1de987ac517e5d979ff37cd
IsuruWeerakkodi <isuruweerakkodi24@gmail.com>
2023-08-13 22:18:47 +0530

    Task-3

---
+++ Task3.java
@@ -0,0 +1,21 @@
+public class Task3 {
+   
+    public static void main(String[] args) throws Exception {
+        int[] nums = {10, 20, 30, 40, 50};
+
+        int[] reverse = new int[nums.length];
+        int i =0;
+
+        for( i=0;i<nums.length;i++){
+        (reverse[nums.length -1 -i])= (nums[(i)]);
+        }
+
+        nums = reverse;
+          System.out.print("[");
+        for (i=0;i<nums.length;i++){
+      
+        System.out.print(nums[i]+ ",");
+        
+        }System.out.println("\b]");
+    }
+}
