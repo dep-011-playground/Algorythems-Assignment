ab67504b2548ed7c8facbcf7f6cba7bec9af8486
IsuruWeerakkodi <isuruweerakkodi24@gmail.com>
2023-08-14 09:13:58 +0530

    Task 4

---
+++ Task4.java
@@ -0,0 +1,19 @@
+public class Task4 {
+    public static void main(String[] args) {
+        int[] nums = {10, 20, 30, 40, 50};
+        int j =0;
+
+        System.out.print("[");
+        for(int i=0;i<nums.length/2;i++){
+            j=nums[nums.length-1-i];
+            nums[nums.length-1-i]=nums[i];
+            nums[i]=j;
+        }
+
+        for(int i=0;i<nums.length;i++){
+        System.out.print(nums[i]+ ",");
+
+        }
+        System.out.println("\b]");
+    }
+}
